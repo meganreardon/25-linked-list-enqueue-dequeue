2dfd2efa853f6cc1260283baa7ddb53452285df2
Megan Reardon <meganr@gmail.com>
2017-01-23 20:47:00 -0800

    whiteboard code methods added

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -1,1 +1,71 @@
 'use strict';
+
+module.exports = SinglyLinkedList;
+
+// ----- CLASS DEMO CODE FROM LAB 24 ----- //
+
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+function SinglyLinkedList(arr) {
+  this.head = null;
+
+  if (Array.isArray(arr)) {
+    arr.forEach( val => {
+      return this.prepend(val);
+    });
+  }
+}
+
+SinglyLinkedList.prototype.prepend = function(val) {
+  let node = new Node(val);
+
+  if (!this.head) {
+    this.head = node;
+    return node.val;
+  }
+
+  node.next = this.head;
+  this.head = node;
+  return node.val;
+};
+
+SinglyLinkedList.prototype.append = function(val) {
+  let node = new Node(val);
+  let lastNode = null;
+
+  if (!this.head) {
+    this.head = node;
+    return node.val;
+  }
+
+  _setLastNode(this.head);
+  lastNode.next = node;
+  return node.val;
+
+  function _setLastNode(node) {
+    if (!node) return;
+    lastNode = node;
+    _setLastNode(node.next);
+  }
+};
+
+// ----- MEGAN AND PETER WHITEBOARD CODE FROM LAB 25 ----- //
+
+SinglyLinkedList.prototype.enqueue = function(val) {
+  let node = new Node(val); // line added, was above outside this function in our whiteboarded code
+  node.next = null;
+  this.tail = node;
+  return node.val;
+};
+
+SinglyLinkedList.prototype.dequeue = function() {
+  if (this.head) {
+    let cur = this.head.next; // added "let", not in whiteboard code
+    this.head = cur;
+    return cur;
+  }
+  return null;
+};
